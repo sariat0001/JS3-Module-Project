8f941fc47784a37c5f69acf8efb4bcde5dce4dac
Sariat <adenike.sariat@gmail.com>
2024-01-24 14:06:40 +0000

    I created the functionality, added template to the html and some styling

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    const card = document.getElementById("tv-shows").content.cloneNode(true);
+    // rootElem.innerHTML = "";
+
+    card.querySelector("h2").textContent = `${episode.name} - S${String(episode.season).padStart(2,0)}E${String(episode.number).padStart(2,0)}`;
+    card.querySelector("img").src = episode.image.medium;
+    card.querySelector("p").innerHTML = episode.summary;
+
+    rootElem.appendChild(card);
+
+    return card;
+  });
 }
 
 window.onload = setup;
